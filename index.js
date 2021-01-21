8f4658c9bee9aee9095b121db3e4818d52727164
Rawle Springer <rspringerdeveloper@gmail.com>
2021-01-21 12:18:10 -0500

    Added New Book button functionality

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ Book.prototype.info = function () {
   return `The title of the book is ${this.name} and it is written by ${this.author}. It has ${pages} pages`;
 };
 function addBookToLibrary() {
-  const title = prompt("What is the title of the book");
-  const author = prompt("What is the name of the author");
-  const pages = prompt("What is the number of pages");
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
   const book = new Book(title, author, pages);
   myLibrary.push(book);
 }
 
-addBookToLibrary();
+// addBookToLibrary();
 
 myLibrary.forEach(({ title, author, pages, read }) => {
   const bookCard = document.createElement("div");
@@ -40,6 +40,41 @@ myLibrary.forEach(({ title, author, pages, read }) => {
   read ? (readEl.textContent = "Yes") : (readEl.textContent = "No");
 
   console.log(readEl.textContent);
+
+  bookCard.appendChild(titleEl);
+  bookCard.appendChild(authorEl);
+  bookCard.appendChild(pagesEl);
+  bookCard.appendChild(readEl);
+
+  booksContainer.appendChild(bookCard);
+});
+
+const addBookButton = document.querySelector(".new-book");
+const form = document.querySelector(".modal");
+const submitButton = document.querySelector(".submit");
+
+addBookButton.addEventListener("click", (e) => {
+  form.style.display = "block";
+});
+
+submitButton.addEventListener("click", () => {
+  addBookToLibrary();
+
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("card");
+  const titleEl = document.createElement("h3");
+  const authorEl = document.createElement("h4");
+  const pagesEl = document.createElement("h4");
+  const readEl = document.createElement("h4");
+
+  const titleText = document.getElementById("title").value;
+  const authorText = document.getElementById("author").value;
+  const pagesText = document.getElementById("pages").value;
+
+  titleEl.textContent = `Title: ${titleText}`;
+  authorEl.textContent = `Author: ${authorText}`;
+  pagesEl.textContent = `Pages: ${pagesText}`;
+  false ? (readEl.textContent = "Yes") : (readEl.textContent = "No");
 
   bookCard.appendChild(titleEl);
   bookCard.appendChild(authorEl);
